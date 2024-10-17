76aa9b43b1eefdb09a7b7a6b7268a5ad692608c3
HectorCastellanosVV <hectorcastellanos429@gmail.com>
2024-10-17 11:44:36 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const sequelize = require('../src/database/database'); 
+const sequelize = require('../src/database/database');
 const app = express();
+const verifyToken = require('../src/middleware/auth');
 
 // Settings
 const PORT = process.env.PORT || 3000;
@@ -15,18 +16,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Rutas
-app.use('/api/users', require('./routes/protected'), require('./routes/users'));
-app.use('/api/menu', require('./routes/protected'), require('./routes/menu'));
-app.use('/api/clientes', require('./routes/protected'), require('./routes/clientes'));
-app.use('/api/pedidos', require('./routes/protected'), require('./routes/pedidos'));
-app.use('/api/login', require('./routes/protected'), require('./routes/login'));
-app.use('/api/personalizar', require('./routes/protected'), require('./routes/ingredientes'));
-app.use('/api/pagos', require('./routes/protected'), require('./routes/pagos'));
-app.use('/api/categorias', require('./routes/protected'), require('./routes/categorias'));
-app.use('/api/bebidas', require('./routes/protected'), require('./routes/bebidas'));
-app.use('/api/mesas', require('./routes/protected'), require('./routes/mesas'));
-app.use('/api/protected', require('./routes/protected'));
+app.use('/api/users', verifyToken, require('./routes/users'));
+app.use('/api/menu', verifyToken, require('./routes/menu'));
+app.use('/api/clientes', verifyToken, require('./routes/clientes'));
+app.use('/api/pedidos', verifyToken, require('./routes/pedidos'));
+app.use('/api/personalizar', verifyToken, require('./routes/ingredientes'));
+app.use('/api/pagos', verifyToken, require('./routes/pagos'));
+app.use('/api/categorias', verifyToken, require('./routes/categorias'));
+app.use('/api/bebidas', verifyToken, require('./routes/bebidas'));
+app.use('/api/mesas', verifyToken, require('./routes/mesas'));
 
+app.use('/api/login', require('./routes/protected'), require('./routes/login'));
+app.use('/api/protected', require('./routes/protected'));
 
 // Starting server
 app.listen(app.get('port'), () => {

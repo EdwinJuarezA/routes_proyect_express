47c7b0cd37475d929e67e1be1f9715df0c722407
HectorCastellanosVV <hectorcastellanos429@gmail.com>
2024-10-31 18:19:21 -0600

    update

--- src/index.js
+++ src/index.js
@@ -3,17 +3,44 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const sequelize = require('../src/database/database');
+const cors = require('cors');
 const app = express();
 const verifyToken = require('../src/middleware/auth');
 
 // Settings
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '192.168.1.162';
 app.set('port', PORT);
+const allowedOrigins = ['http://192.168.1.161',];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('origin: ' + origin);
+
+    if (!origin) {
+      callback(null, true);
+    } else if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
 
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  console.log(`Solicitud recibida desde la IP: ${req.ip}`);
+  next();
+});
+
+
 
 //Rutas
 app.use('/api/users', verifyToken, require('./routes/users'));
@@ -30,8 +57,8 @@ app.use('/api/login', require('./routes/protected'), require('./routes/login'));
 app.use('/api/protected', require('./routes/protected'));
 
 // Starting server
-app.listen(app.get('port'), () => {
-  console.log(`Server on port ${app.get('port')}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
 
 (async () => {
--- src/models/clientes.model.js
+++ src/models/clientes.model.js
@@ -1,33 +1,33 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
+const { DataTypes, Model } = require('sequelize');
 const sequelize = require('../database/database');
 
 
-class Clientes extends Model{}
+class Clientes extends Model { }
 
 Clientes.init({
-  ClienteID:{
+  ClienteID: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true
   },
-  Nombre:{
-    type:DataTypes.STRING,
+  Nombre: {
+    type: DataTypes.STRING,
     allowNull: false //no null
   },
-  Correo:{
-    type:DataTypes.STRING,
-    allowNull:false
+  Correo: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  Telefono:{
-    type:DataTypes.STRING,
-    allowNull:false
+  Telefono: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
-  Comentarios:{
-    type:DataTypes.STRING
+  Comentarios: {
+    type: DataTypes.STRING
   }
-},{
+}, {
   sequelize,
-  modelName:"Clientes",
+  modelName: "Clientes",
 });
 
 module.exports = Clientes;

4f8313d99f7f17f45becdf2030d568c8d1859daf
HectorCastellanosVV <hectorcastellanos429@gmail.com>
2024-11-27 18:02:01 -0600

    update error clientes.js

--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -1,20 +1,22 @@
 const { Router } = require('express');
 const router = Router();
+const clientes = require('../data/clientes.json');
+const { faker } = require('@faker-js/faker'); 
 
 const Clients = require("../models/clientes.model.js");
 
 // Obtener todos los clientes
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
     const clients = await Clients.findAll()
     res.status(200).json({
-        ok: true,
+        ok:true,
         status: 200,
         body: clients
     })
 });
 
 // Obtener un cliente por ID
-router.get('/:id', async (req, res) => {
+router.get('/:id',async (req, res) => {
     const id = req.params.id;
     const client = await Clients.findOne({
         where: {
@@ -22,14 +24,15 @@ router.get('/:id', async (req, res) => {
         }
     });
     res.status(200).json({
-        ok: true,
+        ok:true,
         status: 200,
         body: client
     })
 
-    router.post('/', async (req, res) => {
-    });// Crear un nuevo cliente
+});
 
+// Crear un nuevo cliente
+router.post('/', async(req, res) => {
     const dataClients = req.body;
     await Clients.sync();
     const create_client = await Clients.create({
@@ -42,7 +45,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // Actualizar un cliente
-router.put('/:id', async (req, res) => {
+router.put('/:id', async(req, res) => {
     const id = req.params.id;
     const dataClients = req.body;
     const update_clients = await Clients.update({
@@ -50,20 +53,20 @@ router.put('/:id', async (req, res) => {
         Correo: dataClients.Correo,
         Telefono: dataClients.Telefono,
         Comentarios: dataClients.Comentarios
-    }, {
+    },{
         where: {
             ClienteId: id,
         }
     })
     res.status(200).json({
-        ok: true,
-        status: 200,
-        body: update_clients
+        ok:true,
+        status:200,
+        body:update_clients
     })
 });
 
 // Eliminar un cliente
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async(req, res) => {
     const id = req.params.id;
     const delete_clients = await Clients.destroy({
         where: {
@@ -71,10 +74,8 @@ router.delete('/:id', async (req, res) => {
         }
     })
     res.status(204).json({
-        ok: true,
-        status: 204,
-        body: delete_clients
+        ok:true,
+        status:204,
+        body:delete_clients
     })
 });
-
-module.exports = router;
